87e0a12fcb127c0abe50d98df1f3f1d294311855
Neto Lourenço <otaviojnetoo@gmail.com>
2021-09-26 19:23:45 -0300

    api

--- src/sections/Rent/Rent.tsx
+++ src/sections/Rent/Rent.tsx
@@ -1,37 +1,41 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { CardInfo } from '../../components';
 import { background } from '../../assets';
 
 import { Cards, Container } from './styles';
 import { infosImobiliaria } from '../../services/api';
 
-const getHouse = async () => {
-  try {
-    const { data } = await infosImobiliaria.get();
-  } catch (error) {
-    console.log('error');
-  }
-};
-
-// async function getContent() {
-//   try {
-//     const api = await fetch(
-//       'https://admin-edbe9-default-rtdb.firebaseio.com/',
-//     );
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// console.log(getContent());
-
 const Rent: React.FC = () => {
+  const [house, setHouse] = useState();
+
+  const getHouse = async () => {
+    try {
+      const { data } = await infosImobiliaria.get();
+    } catch (error) {
+      console.log('error');
+    }
+
+    setHouse();
+  };
+
+  // async function getContent() {
+  //   try {
+  //     const api = await fetch(
+  //       'https://admin-edbe9-default-rtdb.firebaseio.com/',
+  //     );
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // console.log(getContent());
+
   return (
     <Container>
       <h1>Imóveis adicionados recentemente</h1>
 
       <Cards>
-        <CardInfo type="{ }" Img={background} />
+        <CardInfo type={house} Img={background} />
       </Cards>
     </Container>
   );
